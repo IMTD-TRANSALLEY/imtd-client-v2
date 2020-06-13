4b4e6a9e00ad0c199972eea713426118d08acbd0
nggrekkodev <nggrekkodev@gmail.com>
2020-06-13 10:12:45 +0200

    init

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
+// const express = require("express");
+// const path = require("path");
+// const app = express();
+
+// // Serve only the static files form the dist directory
+// app.use(express.static("./dist/imtd-client-v2.json"));
+
+// app.get("/*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "/dist/imtd-client-v2/index.html"));
+// });
+
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
+
 const express = require("express");
 const path = require("path");
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static("./dist/imtd-client-v2.json"));
+// Serve static files....
+app.use(express.static(__dirname + "/dist/imtd-client-v2"));
 
+// Send all requests to index.html
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "/dist/imtd-client-v2/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/imtd-client-v2/index.html"));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// default Heroku PORT
+app.listen(process.env.PORT || 3000);
